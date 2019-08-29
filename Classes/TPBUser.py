8b1e78da76017bd7ec43590d9f6ce8e9ddcbfe77
Vadim <StepanenkoVK@Gmail.com>
2019-08-29 23:00:38 +0300

    remove temp file, WIP on new parsing

--- Classes/TPBUser.py
+++ Classes/TPBUser.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 from html.parser import HTMLParser
 from Classes.Torrent import Torrent
@@ -7,6 +8,8 @@ from Classes.Torrent import Torrent
 # This class will hopefully contain methods to login into sites and get magnet links / torrent files from those
 class TPBUser(object):
     URL = "https://thepiratebay.icu"
+    LINE_REGEX = re.compile(
+        r'.*"Details.*">(?<Name>.*)<\/a.*<a href="(?<Link>.*)" title.*Size (?<Size>.*), UL.*<td align="right">(?<Seeders>\d*)<\/td>.*">(?<Leechers>\d*)<.*')
 
     # 0/99/401 = unknown(relevancy?)/torrents availability?/category(games -> PC)
     def __construct_search_url(self, game_name):
@@ -45,29 +48,21 @@ class TPBUser(object):
                 list_of_torrents.append(temp_torrent)
         return list_of_torrents
 
-    # Rework of __parse_search_result using html.parser
     def __parse_search_result_new(self, result):
-        parser = TPBParser()
-        parser.feed(result)
-        return parser.get_torrents()
+        LINE_START = '<div class="detName">'
+        LINE_FINISH = '<td class="vertTh">'
+        torrentLines = []
+        text = self.__remove_useless_HTML(result)
+        while text.find(LINE_START) > -1:
+            torrentLines.append(
+                text[text.find(LINE_START): text.find(LINE_FINISH)])
+            text = text[LINE_FINISH + len(LINE_FINISH):]
+        return [self.__convert_HTML_into_torrent(line) for line in torrentLines]
+
+    def __convert_HTML_into_torrent(self, line):
+        match = re.match(self.LINE_REGEX, line)
+        pass
 
     def get_torrents(self, game_name):
         r = requests.get(self.__construct_search_url(game_name))
         return self.__parse_search_result_new(r.text)
-
-
-class TPBParser(HTMLParser):
-    torrents = []
-    currentTorrent = None
-
-    def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
-
-    def handle_endtag(self, tag):
-        print("Encountered an end tag :", tag)
-
-    def handle_data(self, data):
-        print("Encountered some data  :", data)
-
-    def get_torrents(self):
-        return self.torrents
